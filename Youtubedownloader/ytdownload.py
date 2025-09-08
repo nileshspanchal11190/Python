29b877a6c3d7a9a273bee21534140641b9a3dcfd
Nilesh Panchal <76562653+nileshspanchal11190@users.noreply.github.com>
2025-09-08 15:10:53 +0000

    Add files via upload

---
+++ Youtubedownloader/ytdownload.py
@@ -0,0 +1,20 @@
+from pytubefix import YouTube
+
+try:
+    # Ask the user to input the YouTube URL
+    url = input("Enter the YouTube URL: ")
+    
+    yt = YouTube(url)  # Example URL
+    
+    print("Title:", yt.title)
+    print("Views:", yt.views)
+
+    # Get the highest resolution stream
+    yd = yt.streams.get_highest_resolution()
+    
+    # Download the video to the current directory
+    yd.download()
+    
+    print("Download complete.")
+except Exception as e:
+    print("An error occurred:", str(e))
